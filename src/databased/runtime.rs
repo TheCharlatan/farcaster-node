ed1fe7fffa550b8f3e082cc8e4b7d9c5bb3f7eb8
TheCharlatan <seb.kung@gmail.com>
2022-08-30 12:56:52 +0200

    Restore checkpoint: Use cli to drive execution
    Instead of keeping track of the available checkpoints in the farcaster
    runtime, we use the cli to drive execution by first retrieving the
    checkpoint information for a certain swap id and then triggering the
    restore workflow.
    
    Because the checkpoint list does not need to be persisted in farcasterd
    anymore, the checkpoint list is no longer routed through farcasterd.
    
    Update the docs to reflect the change.

--- src/databased/runtime.rs
+++ src/databased/runtime.rs
@@ -19,7 +19,7 @@ use crate::rpc::{
 };
 use crate::{CtlServer, Error, Service, ServiceConfig, ServiceId};
 use internet2::TypedEnum;
-use microservices::esb;
+use microservices::esb::{self, Handler};
 
 pub fn run(config: ServiceConfig, data_dir: PathBuf) -> Result<(), Error> {
     let runtime = Runtime {
@@ -121,7 +121,7 @@ impl Runtime {
                 debug!("checkpoint set");
             }
 
-            Request::RestoreCheckpoint(swap_id) => {
+            Request::RestoreCheckpoint(CheckpointEntry { swap_id, .. }) => {
                 match self.database.get_checkpoint_state(&CheckpointKey {
                     swap_id,
                     service_id: ServiceId::Wallet,
@@ -227,10 +227,79 @@ impl Runtime {
                     .collect();
                 endpoints.send_to(
                     ServiceBus::Ctl,
+                    self.identity(),
                     source,
-                    ServiceId::Farcasterd,
                     Request::CheckpointList(checkpointed_pub_offers),
                 )?;
+            }
+
+            Request::GetCheckpointEntry(swap_id) => {
+                match self.database.get_checkpoint_state(&CheckpointKey {
+                    swap_id,
+                    service_id: ServiceId::Wallet,
+                }) {
+                    Ok(raw_state) => {
+                        match CheckpointState::strict_decode(std::io::Cursor::new(raw_state)) {
+                            Ok(CheckpointState::CheckpointWallet(CheckpointWallet {
+                                wallet,
+                                ..
+                            })) => match wallet {
+                                Wallet::Bob(wallet) => {
+                                    endpoints.send_to(
+                                        ServiceBus::Ctl,
+                                        self.identity(),
+                                        source,
+                                        Request::CheckpointEntry(CheckpointEntry {
+                                            swap_id,
+                                            public_offer: wallet.pub_offer,
+                                            trade_role: wallet.local_trade_role,
+                                        }),
+                                    )?;
+                                }
+                                Wallet::Alice(wallet) => {
+                                    endpoints.send_to(
+                                        ServiceBus::Ctl,
+                                        self.identity(),
+                                        source,
+                                        Request::CheckpointEntry(CheckpointEntry {
+                                            swap_id,
+                                            public_offer: wallet.pub_offer,
+                                            trade_role: wallet.local_trade_role,
+                                        }),
+                                    )?;
+                                }
+                            },
+                            _ => {
+                                endpoints.send_to(
+                                    ServiceBus::Ctl,
+                                    self.identity(),
+                                    source,
+                                    Request::Failure(Failure {
+                                        code: FailureCode::Unknown,
+                                        info: format!(
+                                            "Could not retrieve checkpoint entry for {}",
+                                            swap_id
+                                        ),
+                                    }),
+                                )?;
+                            }
+                        }
+                    }
+                    _ => {
+                        endpoints.send_to(
+                            ServiceBus::Ctl,
+                            self.identity(),
+                            source,
+                            Request::Failure(Failure {
+                                code: FailureCode::Unknown,
+                                info: format!(
+                                    "Could not retrieve checkpoint entry for {}",
+                                    swap_id
+                                ),
+                            }),
+                        )?;
+                    }
+                }
             }
 
             Request::RemoveCheckpoint(swap_id) => {
